faafeec302c148dfe1c7a556a2be34754b1ff324
rickborne <>
2025-11-13 21:22:49 +0000

    Combining Conditions

---
+++ conditional_statements/combining_conditions/main.py
@@ -0,0 +1,13 @@
+# Evaluate if an item is discounted or low in stock to determine its eligibility for promotion.
+
+# Define a boolean variable movingProduct that is True if the item is either discounted or low in stock,
+# using logical operators.
+# Create a boolean variable promotion that is True if the item is not discounted and sufficiently
+# stocked.
+# Print the message: Is the item eligible for promotion? <promotion>.
+# Output Requirements
+# Print whether the item is eligible for promotion: Is the item eligible for promotion? <promotion>.
+
+# The item's discount and stock status have been defined
+discounted = False
+lowStock = True
